b7cc926bf51af3ed8ea690a0f51e86c4f445de03
Sherlocky <y836097668@126.com>
2019-06-17 17:23:54 +0800

    启动后默认执行一次同步任务

--- src/main/java/com/sherlocky/qiniusyncnas/QiniuSyncNasApplication.java
+++ src/main/java/com/sherlocky/qiniusyncnas/QiniuSyncNasApplication.java
@@ -1,12 +1,22 @@
 package com.sherlocky.qiniusyncnas;
 
+import com.sherlocky.qiniusyncnas.entity.SyncResult;
+import com.sherlocky.qiniusyncnas.service.QiniuSyncNasService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @SpringBootApplication
 @EnableScheduling
-public class QiniuSyncNasApplication {
+public class QiniuSyncNasApplication implements CommandLineRunner {
+    @Autowired
+    private QiniuSyncNasService service;
 
     public static void main(String[] args) {
         SpringApplication.run(QiniuSyncNasApplication.class, args);
@@ -25,4 +35,13 @@ public class QiniuSyncNasApplication {
         return taskScheduler;
     }
     */
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.error("启动后默认执行一次同步任务：");
+        log.error("###### 七牛文件同步任务开始", LocalDateTime.now());
+        SyncResult r = service.sync();
+        log.error("###### 同步结果：" + r);
+        log.info("###### 七牛文件同步任务结束~", LocalDateTime.now());
+    }
 }
